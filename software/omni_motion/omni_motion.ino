773b8b461186f82240d1aece1be976737c5140e0
Sunmin Hwang <trsch902@gmail.com>
2025-08-06 16:47:46 +1000

    Update omni_motion.ino
    I just simplified it kings (_ _)

--- software/omni_motion/omni_motion.ino
+++ software/omni_motion/omni_motion.ino
@@ -2,21 +2,7 @@
 #include <SoftwareSerial.h>
 #include <MotorClass.h>
 
-const float pi = 3.14159265358979323846;
-
-int pins[4][3] = {
-  {4, 3, 5},
-  {8, 7, 9},
-  {12, 11, 13},
-  {16, 15, 17}
-}; // pin numbers
-
-MotorClass omnimotion(pins);
-
-
-// const int dirPin0 = 4;
-// const int pwmPin0 = 3;
-// const int brakePin0 = 5;
+MotorClass omnimotion();
 
 
 void setup() {
