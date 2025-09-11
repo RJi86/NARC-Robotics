3f1a9b00727a61031168b04742ea470089a5b980
Richard Ji <jirichard2012@gmail.com>
2025-09-11 10:41:37 +1000

    changed name to controllers

--- software/multiplexer/R_Multiplexer/Pins.h
+++ software/multiplexer/R_Multiplexer/Pins.h
@@ -3,19 +3,19 @@
 
 namespace pins {
 // 0-7
-constexpr uint8_t MUX0_SIG = A0; //sig
-constexpr uint8_t MUX0_S0 = 2; 
-constexpr uint8_t MUX0_S1 = 3; 
-constexpr uint8_t MUX0_S2 = 4; 
+constexpr uint8_t MUX0_SIG = 8; //sig
+constexpr uint8_t MUX0_S0 = 1; 
+constexpr uint8_t MUX0_S1 = 16; 
+constexpr uint8_t MUX0_S2 = 15 
 // 8-15
-constexpr uint8_t MUX1_SIG = A1;
-constexpr uint8_t MUX1_S0 = 5;
-constexpr uint8_t MUX1_S1 = 6;
-constexpr uint8_t MUX1_S2 = 7;
+constexpr uint8_t MUX1_SIG = 8;
+constexpr uint8_t MUX1_S0 = 1;
+constexpr uint8_t MUX1_S1 = 16;
+constexpr uint8_t MUX1_S2 = 15;
 
 // 16-23
-constexpr uint8_t MUX2_SIG = A2;
-constexpr uint8_t MUX2_S0 = 8;
-constexpr uint8_t MUX2_S1 = 9;
-constexpr uint8_t MUX2_S2 = 10;
+constexpr uint8_t MUX2_SIG = 8;
+constexpr uint8_t MUX2_S0 = 1;
+constexpr uint8_t MUX2_S1 = 16;
+constexpr uint8_t MUX2_S2 = 15;
 }
